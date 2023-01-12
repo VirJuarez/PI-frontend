a5cd7f2db394f9618d0581d1d224a7625fc006bc
Virignia <virginia.juarez1996@gmail.com>
2023-01-12 15:44:12 -0300

    cambiosdeploy vercel

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import axios from "axios"
+import dotenv from "dotenv"
+dotenv.config()
 
 // axios.defaults.baseURL = "http://localhost:3001"
 
-axios.defaults.baseURL = "https://pi-backend-production.up.railway.app/"
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001"
 
 ReactDOM.render(
   <Provider store={store}>
